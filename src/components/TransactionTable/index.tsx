0a135a83b84f8cb9e8edc874fbebb098b20d6ca2
WillAngelis <willangelis@gmail.com>
2022-07-28 14:25:32 -0300

    add trasanction components

---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from 'react';
+import { api } from '../../services/api';
+import { Container } from './styles';
+
+export function TransactionTable() {
+  useEffect(() => {
+    api
+      .get('/transactions')
+
+      .then((response) => console.log(response.data));
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Websites</td>
+            <td className="deposit">12,000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de Aplicativos</td>
+            <td className="withdraw">- 8,000</td>
+            <td>Desenvolvimento</td>
+            <td>10/02/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
